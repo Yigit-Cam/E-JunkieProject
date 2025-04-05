6304762ab780f78a52f81fe305c939add3520ee1
zaferatakli <zatakli@gmail.com>
2025-04-06 05:58:23 +1000

    XML classes tested and example classes tested.

--- src/Utility/MyFunc.java
+++ src/Utility/MyFunc.java
@@ -1,19 +1,19 @@
 package Utility;
 
 public class MyFunc {
-    public static void Sleep(int second){
+    public static void Sleep(int second) {
         try {
-            Thread.sleep(second*1000);
+            Thread.sleep(second * 1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
-    public static int GetRndmNumber(int min, int max)
-    {
-        return  (int)(Math.random()* (max-min) )+ min;
+
+    public static int GetRandomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min)) + min;
     }
-    public static int GetRndmNumber(int max)
-    {
-        return  (int)(Math.random()* max);
+
+    public static int GetRandomNumber(int max) {
+        return (int) (Math.random() * max);
     }
 }
