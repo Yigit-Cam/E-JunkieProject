8fd06d5ec72216c57e03f57dc085e68fd6b524ad
PC <nroztrk17@gmail.com>
2025-04-07 21:11:24 +0300

    Element and method classes have been written and Automation codes have been written.

--- src/eJunkie/methods/InformationVideoMethods.java
+++ src/eJunkie/methods/InformationVideoMethods.java
@@ -1,14 +1,17 @@
 package eJunkie.methods;
-
+import Utility.MyFunc;
+import eJunkie.elements.InformationVideoElements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class InformationVideoMethods {
     WebDriver driver;
     Actions action;
     WebDriverWait wait;
-    InformationVideoMethods informationVideoMethods;
+    InformationVideoElements informationVideoElements;
 
     public InformationVideoMethods(WebDriver driver, Actions action, WebDriverWait wait) {
         this.driver = driver;
@@ -17,6 +20,26 @@ public class InformationVideoMethods {
     }
 
     public void playVideo() {
-        informationVideoMethods = new InformationVideoMethods(driver, action, wait);
+        informationVideoElements = new InformationVideoElements(driver);
+
+        wait.until(ExpectedConditions.urlToBe("https://shopdemo.fatfreeshop.com/"));
+
+        action.click(informationVideoElements.eCommerceButton).perform();
+
+        MyFunc.Sleep(2);
+
+        driver.navigate().refresh();
+
+        wait.until(ExpectedConditions.elementToBeClickable(informationVideoElements.howItWorksButton));
+
+        action.click(informationVideoElements.howItWorksButton).perform();
+
+        wait.until(ExpectedConditions.elementToBeClickable(informationVideoElements.playButton)).click();
+
+        MyFunc.Sleep(11);
+
+        wait.until(ExpectedConditions.elementToBeClickable(informationVideoElements.closeButton)).click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("https://www.e-junkie.com/"), "URL is not correct");
     }
 }
