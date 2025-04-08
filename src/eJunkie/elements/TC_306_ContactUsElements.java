4c7c597191bec0fcf73615b4d55a968dc54dc46b
zaferatakli <zatakli@gmail.com>
2025-04-08 16:24:44 +1000

    Classes name changed.

--- src/eJunkie/elements/TC_306_ContactUsElements.java
+++ src/eJunkie/elements/TC_306_ContactUsElements.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class ContactUsElements {
+public class TC_306_ContactUsElements {
 
-    public ContactUsElements(WebDriver driver) {
+    public TC_306_ContactUsElements(WebDriver driver) {
         PageFactory .initElements(driver, this);
     }
 
--- src/eJunkie/methods/TC_306_ContactUsMethods.java
+++ src/eJunkie/methods/TC_306_ContactUsMethods.java
@@ -1,26 +1,26 @@
 package eJunkie.methods;
 
-import eJunkie.elements.ContactUsElements;
+import eJunkie.elements.TC_306_ContactUsElements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-public class ContactUsMethods {
+public class TC_306_ContactUsMethods {
     WebDriver driver;
     Actions action;
     WebDriverWait wait;
-    ContactUsElements contactUsElements;
+    TC_306_ContactUsElements elements;
 
-    public ContactUsMethods(WebDriver driver, Actions action, WebDriverWait wait) {
+    public TC_306_ContactUsMethods(WebDriver driver, Actions action, WebDriverWait wait) {
         this.driver = driver;
         this.action = action;
         this.wait = wait;
     }
 
     public void sendMessage(){
-        contactUsElements = new ContactUsElements(driver);
+        elements = new TC_306_ContactUsElements(driver);
         String name = "Bug Fathers";
         String email = "cigeyab754@erapk.com";
         String subject = "Test Subject";
@@ -30,23 +30,22 @@ public class ContactUsMethods {
         Assert.assertTrue(driver.getCurrentUrl().contains("https://shopdemo.fatfreeshop.com/"), "URL is not correct");
 
         action.scrollByAmount(0, 500)
-                .click(contactUsElements.contactUsButton)
+                .click(elements.contactUsButton)
                 .build()
                 .perform();
 
         wait.until(ExpectedConditions.urlToBe("https://shopdemo.fatfreeshop.com/contact"));
         Assert.assertTrue( driver.getCurrentUrl().contains("https://shopdemo.fatfreeshop.com/contact"), "URL is not correct");
 
-        wait.until(ExpectedConditions.visibilityOf(contactUsElements.nameInput)).sendKeys(name);
-        wait.until(ExpectedConditions.visibilityOf(contactUsElements.emailInput)).sendKeys(email);
-        wait.until(ExpectedConditions.visibilityOf(contactUsElements.subjectInput)).sendKeys(subject);
-        wait.until(ExpectedConditions.visibilityOf(contactUsElements.messageInput)).sendKeys(message);
-        wait.until(ExpectedConditions.elementToBeClickable(contactUsElements.sendMessageButton)).click();
+        wait.until(ExpectedConditions.visibilityOf(elements.nameInput)).sendKeys(name);
+        wait.until(ExpectedConditions.visibilityOf(elements.emailInput)).sendKeys(email);
+        wait.until(ExpectedConditions.visibilityOf(elements.subjectInput)).sendKeys(subject);
+        wait.until(ExpectedConditions.visibilityOf(elements.messageInput)).sendKeys(message);
+        wait.until(ExpectedConditions.elementToBeClickable(elements.sendMessageButton)).click();
 
         wait.until(ExpectedConditions.alertIsPresent());
         String alertText = driver.switchTo().alert().getText();
         System.out.println("Alert text: " + alertText);
         driver.switchTo().alert().accept();
-
     }
 }
